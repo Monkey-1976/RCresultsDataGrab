08b0e263452e1a4dc9acbb885d12eb1b777912e4
Monkey-1976 <andyplatt76@gmail.com>
2024-01-18 17:42:42 +0000

    Added best 10

--- Program.cs
+++ Program.cs
@@ -37,60 +37,60 @@ class Program
             {
                 try
                 {
- raceResults = WebScrapper.GetRaceResults("https://www.rc-results.com" + race);
+                    raceResults = WebScrapper.GetRaceResults("https://www.rc-results.com" + race);
 
-                foreach (RaceResult result in raceResults)
-                {
-
-                    OverallResult? overallResult = meeting.OverallResults.FirstOrDefault(o => o.Driver == result.Driver);
-
-                    string[] raceLaps = result.Result.Split('/');
-                    TotalLaps totalLaps = new TotalLaps
+                    foreach (RaceResult result in raceResults)
                     {
-                        Laps = Int32.TryParse(raceLaps[0].Trim(' '), out int numLaps) ? numLaps : 0,
-                        TotalTime = float.TryParse(raceLaps[1].Trim(' '), out float time) ? time : 99999
-                    };
 
-                    if (overallResult == null)
-                    {
-                        overallResult = new OverallResult
+                        OverallResult? overallResult = meeting.OverallResults.FirstOrDefault(o => o.Driver == result.Driver);
+
+                        string[] raceLaps = result.Result.Split('/');
+                        TotalLaps totalLaps = new TotalLaps
                         {
-                            Driver = result.Driver,
-                            Class = meeting.Name,
-                            BestLap = float.TryParse(result.Best, out float bestLap) ? bestLap : 99999,
-                            Best10 = float.TryParse(result.Best10, out float best10) ? best10 : 99999,
-                            BestLaps = totalLaps
+                            Laps = Int32.TryParse(raceLaps[0].Trim(' '), out int numLaps) ? numLaps : 0,
+                            TotalTime = float.TryParse(raceLaps[1].Trim(' '), out float time) ? time : 99999
                         };
 
-                        meeting.OverallResults.Add(overallResult);
+                        if (overallResult == null)
+                        {
+                            overallResult = new OverallResult
+                            {
+                                Driver = result.Driver,
+                                Class = meeting.Name,
+                                BestLap = float.TryParse(result.Best, out float bestLap) ? bestLap : 99999,
+                                Best10 = float.TryParse(result.Best10, out float best10) ? best10 : 99999,
+                                BestLaps = totalLaps
+                            };
+
+                            meeting.OverallResults.Add(overallResult);
+                        }
+                        else
+                        {
+                            float newBest = float.TryParse(result.Best, out float bestLap) ? bestLap : 99999;
+
+                            if ((newBest < overallResult.BestLap) && (newBest > 0))
+                            {
+                                overallResult.BestLap = newBest;
+                            }
+
+                            float newBest10 = float.TryParse(result.Best10, out float best10) ? best10 : 99999;
+
+                            if ((newBest10 < overallResult.Best10) && (newBest10 > 0))
+                            {
+                                overallResult.Best10 = newBest10;
+                            }
+
+
+                            if (totalLaps.Laps > overallResult.BestLaps.Laps)
+                            {
+                                overallResult.BestLaps = totalLaps;
+                            }
+                            else if ((overallResult.BestLaps.Laps == totalLaps.Laps) && (totalLaps.TotalTime < overallResult.BestLaps.TotalTime))
+                            {
+                                overallResult.BestLaps = totalLaps;
+                            }
+                        }
                     }
-                    else
-                    {
-                        float newBest = float.TryParse(result.Best, out float bestLap) ? bestLap : 99999;
-
-                        if ((newBest < overallResult.BestLap) && (newBest > 0))
-                        {
-                            overallResult.BestLap = newBest;
-                        }
-
-                        float newBest10 = float.TryParse(result.Best10, out float best10) ? best10 : 99999;
-
-                        if((newBest10 < overallResult.Best10) && (newBest10 > 0))
-                        {
-                            overallResult.Best10 = newBest10;
-                        }
-
-
-                        if (totalLaps.Laps > overallResult.BestLaps.Laps)
-                        {
-                            overallResult.BestLaps = totalLaps;
-                        }
-                        else if ((overallResult.BestLaps.Laps == totalLaps.Laps) && (totalLaps.TotalTime < overallResult.BestLaps.TotalTime))
-                        {
-                            overallResult.BestLaps = totalLaps;
-                        }
-                    }
-                }
                 }
                 catch
                 {
@@ -106,9 +106,10 @@ class Program
         {
             _logger.LogInformation($"{meeting.Name} - {meeting.Date} - {meeting.Url}");
 
-          OutputByFastestLap(meeting);
-          OutputByLaps(meeting);
-          OutputByDiffBestAverage(meeting);
+            OutputByFastestLap(meeting);
+            OutputByLaps(meeting);
+            OutputByDiffBestAverage(meeting);
+            OutputByBest10(meeting);
         }
 
         _logger.LogInformation("Completed.");
@@ -136,7 +137,7 @@ class Program
         file.Write($"Average Best Lap {totalTime / position}");
     }
 
-      private static void OutputByLaps(RaceMeeting meeting)
+    private static void OutputByLaps(RaceMeeting meeting)
     {
         var file = new WriteFile(_filePath + meeting.Name.Replace(" ", "") + "_Laps_" + meeting.Date.ToShortDateString().Replace("/", ""));
 
@@ -157,7 +158,7 @@ class Program
         file.Write($"Average Laps {totalLaps / position}");
     }
 
-     private static void OutputByDiffBestAverage(RaceMeeting meeting)
+    private static void OutputByDiffBestAverage(RaceMeeting meeting)
     {
         var file = new WriteFile(_filePath + meeting.Name.Replace(" ", "") + "_BestAverage_" + meeting.Date.ToShortDateString().Replace("/", ""));
 
@@ -173,6 +174,21 @@ class Program
         }
     }
 
+    private static void OutputByBest10(RaceMeeting meeting)
+    {
+        var file = new WriteFile(_filePath + meeting.Name.Replace(" ", "") + "_Best10_" + meeting.Date.ToShortDateString().Replace("/", ""));
+
+        int position = 1;
+        foreach (OverallResult result in meeting.OverallResults.OrderBy(o => o.Best10))
+        {
+            if (result.Best10 > 0)
+            {
+                _logger.LogInformation($"Position {position.ToString("00")} | {result.Driver},{result.BestLap},{result.Best10},{result.BestLaps.Laps},{result.BestLaps.TotalTime}");
+                file.Write($"{result.Driver}: {result.Best10}");
+                position++;
+            }
+        }
+    }
 
     private const string _filePath = "datafiles/";
     private static ILogger _logger = Log.Logger;
